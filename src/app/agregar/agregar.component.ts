1cf45bf9a1cb4b5052e565e30520a275a0aaaafb
katamus <christian.catamuscay.h@gmail.com>
2021-10-13 20:02:09 -0500

    69. formulario  bindea con el modulo ReactiveForms con el tipo de objeto FormGroup y su creacion de reglas que aplican al formulario

--- src/app/agregar/agregar.component.ts
+++ src/app/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  formularioCreado!: FormGroup;
+
+  constructor(private formBuilder:FormBuilder) { 
+
+  }
 
   ngOnInit(): void {
+    this.crearFormulario();
+  }
+
+  crearFormulario(){
+    this.formularioCreado = this.formBuilder.group({
+      nombre:['Carmen',Validators.required],
+      correo:['',Validators.compose([
+        Validators.required,Validators.email
+      ])],
+      password:['',Validators.compose([
+        Validators.required,Validators.minLength(8)
+      ])]
+    })
   }
 
 }
