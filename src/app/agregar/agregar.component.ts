8d1caf223b18772e284012cfed3d630e6b29eb0b
katamus <christian.catamuscay.h@gmail.com>
2021-10-13 21:10:43 -0500

    70. validaciones con el modulo reactiveform

--- src/app/agregar/agregar.component.ts
+++ src/app/agregar/agregar.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+interface Usuario{
+  nombre:string,
+  correo:string,
+  password:string
+}
+
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
   styleUrls: ['./agregar.component.scss']
 })
 export class AgregarComponent implements OnInit {
+
+  usuarios:Array<Usuario> = new Array<Usuario>();
 
   formularioCreado!: FormGroup;
 
@@ -28,6 +36,11 @@ export class AgregarComponent implements OnInit {
         Validators.required,Validators.minLength(8)
       ])]
     })
+  }
+
+  agregar(){
+    this.usuarios.push(this.formularioCreado.value as Usuario);
+    this.formularioCreado.reset();
   }
 
 }
